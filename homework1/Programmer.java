4b70e77c42dade7045a6d6651fd8b928f8d06163
Kai Vermeren <vermerenkai@gmail.com>
2025-10-07 00:07:43 +0300

    Add homework1, homework2

--- homework1/Programmer.java
+++ homework1/Programmer.java
@@ -1,39 +1,107 @@
 package school.xset.lesson1_What_is_Java.homework1;
 
 class Programmer {
-    // TODO: Создать класс Programmer с полями:
+    // класс Programmer с полями:
     //       - String name - имя программиста
     //       - int age - возраст программиста
     //       - boolean isHasTask - наличие активной задачи
     //       - int tiredness - уровень усталости
     //       - boolean isKnowJava - знает ли программист Java
+    String name;
+    int age;
+    boolean isHasTask;
+    int tiredness;
+    boolean isKnowJava;
 
-    // TODO: Реализовать конструктор Programmer(String name, int age, boolean isHasTask, int tiredness, boolean isKnowJava):
-    //       - Инициализировать все поля переданными значениями
+    // конструктор Programmer(String name, int age, boolean isHasTask, int tiredness, boolean isKnowJava):
+    public Programmer(String name, int age, boolean isHasTask, int tiredness, boolean isKnowJava) {
+        this.name = name;
+        this.age = age;
+        this.isHasTask = isHasTask;
+        this.tiredness = tiredness;
+        this.isKnowJava = isKnowJava;
+    }
 
-    // TODO: Реализовать метод goWork():
-    //       - Если у программиста есть задача, вывести сообщение "[name] работает над задачей." и увеличить tiredness на 1
-    //       - Если задачи нет, вывести сообщение "[name] не имеет активных задач, и пошел ее получать" и вызвать getTask()
+    // метод goWork():
+    //       - Если у программиста есть задача, выводит сообщение "[name] работает над задачей." и увеличить tiredness на 1
+    //       - Если задачи нет, выводит сообщение "[name] не имеет активных задач, и пошел ее получать" и вызвать getTask()
+    public void goWork() {
+        if (isHasTask) {
+            System.out.println(name + " работает над задачей.");
+            tiredness++;
+        } else {
+            System.out.println(name + " не имеет активных задач, и пошел ее получать");
+            getTask();
+        }
+    }
+    // метод getTask():
+    //       - Устанавливает isHasTask в true
+    public void getTask() {
+        isHasTask = true;
+    }
 
-    // TODO: Реализовать метод getTask():
-    //       - Установить isHasTask в true
+    // метод finishTask():
+    //       - Устанавливает isHasTask в false
+    public void finishTask() {
+        isHasTask = false;
+    }
 
-    // TODO: Реализовать метод finishTask():
-    //       - Установить isHasTask в false
+    // метод goSleep():
+    //       - Выводит сообщение "[name] идет спать."
+    //       - Уменьшает tiredness на 1
+    public void goSleep() {
+        System.out.println(name + " идет спать.");
+        tiredness--;
+    }
 
-    // TODO: Реализовать метод goSleep():
-    //       - Вывести сообщение "[name] идет спать."
-    //       - Уменьшить tiredness на 1
-
-    // TODO: Реализовать метод teachJava(Programmer anotherProgrammer):
+    // метод teachJava(Programmer anotherProgrammer):
     //       - Если текущий программист знает Java, а другой нет:
-    //         - Обучить другого программиста (установить isKnowJava в true)
-    //         - Вывести сообщение "[name] обучил [anotherProgrammer.name] языку Java."
-    //       - Если текущий программист не знает Java, вывести "[name] не знает Java и не может обучить [anotherProgrammer.name]."
-    //       - Если второй программист уже знает Java, вывести "[anotherProgrammer.name] уже знает Java."
+    //         - Обучает другого программиста (установить isKnowJava в true)
+    //         - Выводит сообщение "[name] обучил [anotherProgrammer.name] языку Java."
+    //       - Если текущий программист не знает Java, выводит "[name] не знает Java и не может обучить [anotherProgrammer.name]."
+    //       - Если второй программист уже знает Java, выводит "[anotherProgrammer.name] уже знает Java."
+    public void teachJava(Programmer anotherProgrammer) {
+        if (isKnowJava && !anotherProgrammer.isKnowJava) {
+            anotherProgrammer.isKnowJava = true;
+            System.out.println(name + " обучил " + anotherProgrammer.name + " языку Java.");
+            tiredness++;
+        } else if (!isKnowJava) {
+            System.out.println(name + " не знает Java и не может обучить " + anotherProgrammer.name + ".");
+            getTask();
+        } else {
+            System.out.println(anotherProgrammer.name + " уже знает Java.");
+        }
+    }
 
-    // TODO: Проверить отработку методов в main
-    //       - Создать двух программистов с разными характеристиками
-    //       - Проверить работу всех методов класса
+    // проверка отработки методов в main
+    //       - Создание двух программистов с разными характеристиками
+    //       - Проверка работы всех методов класса
+    public static void main(String[] args) {
+        Programmer proger1 = new Programmer("Abraham", 34, false, 7, true);
+        Programmer proger2 = new Programmer("Bella", 23, true, 5, false);
+
+        proger1.goWork();
+        proger2.goWork();
+
+        proger1.getTask();
+        proger2.getTask();
+
+        proger1.goWork();
+        proger2.goWork();
+
+        proger1.finishTask();
+        proger2.finishTask();
+
+        proger1.goWork();
+        proger2.goWork();
+
+        proger1.goSleep();
+        proger2.goSleep();
+
+        proger2.teachJava(proger1);
+        proger1.teachJava(proger2);
+        proger2.teachJava(proger1);
+
+    }
 
 }
--- homework2/Lesson2HWController.java
+++ homework2/Lesson2HWController.java
@@ -1,27 +1,63 @@
 package school.xset.lesson1_What_is_Java.homework2;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Random;
+
+// класс Lesson2HWController с аннотацией @RestController для обработки HTTP запросов
+@RestController
 public class Lesson2HWController {
+    // метод currentTime():
+    //       - Используя LocalDateTime.now(), получаем текущее время
+    //       - Возвращаем строку с текущим временем в формате "Текущее время: [currentTime]"
+    //       - Метод доступен по GET запросу на "/current-datetime"
+    @GetMapping("/current-datetime")
+    public String currentTime() {
+        LocalDateTime now = LocalDateTime.now();
+        return "Текущее время: " + now;
+    }
 
-    // TODO: Создать класс Lesson2HWController с аннотацией @RestController для обработки HTTP запросов
-
-    // TODO: Реализовать метод currentTime():
-    //       - Используя LocalDateTime.now(), получить текущее время
-    //       - Вернуть строку с текущим временем в формате "Текущее время: [currentTime]"
-    //       - Метод должен быть доступен по GET запросу на "/current-datetime"
-
-    // TODO: Реализовать метод currentSeason():
-    //       - Получить текущий месяц через LocalDate.now().getMonthValue()
-    //       - В зависимости от месяца вернуть сезон:
+    // метод currentSeason():
+    //       - Получаем текущий месяц через LocalDate.now().getMonthValue()
+    //       - В зависимости от месяца возвращаем сезон:
     //         - Зима (декабрь, январь, февраль)
     //         - Весна (март, апрель, май)
     //         - Лето (июнь, июль, август)
     //         - Осень (сентябрь, октябрь, ноябрь)
-    //       - Метод должен быть доступен по GET запросу на "/current-season"
+    //       - Метод доступен по GET запросу на "/current-season"
+    @GetMapping("/current-season")
+    public String currentSeason() {
+        LocalDate now = LocalDate.now();
+        int month = now.getMonthValue();
+        String season;
+        if (month == 12 || month == 1 || month == 2) {
+            season = "Зима";
+        } else if (month >= 3 && month <= 5) {
+            season = "Весна";
+        } else if (month >= 6 && month <= 8) {
+            season = "Лето";
+        } else {
+            season = "Осень";
+        }
+        return season;
+    }
 
-    // TODO: Реализовать метод futureDate():
-    //       - Получить текущую дату через LocalDate.now()
-    //       - Сгенерировать случайное количество дней в пределах 30 (с использованием Random)
-    //       - Добавить эти дни к текущей дате для получения будущей даты
-    //       - Вернуть строку с будущей датой в формате "Случайная дата в будущем: [futureDate]"
-    //       - Метод должен быть доступен по GET запросу на "/future-date"
+    // метод futureDate():
+    //       - Получаем текущую дату через LocalDate.now()
+    //       - Генерируем случайное количество дней в пределах 30 (с использованием Random)
+    //       - Добавляем эти дни к текущей дате для получения будущей даты
+    //       - Возвращаем строку с будущей датой в формате "Случайная дата в будущем: [futureDate]"
+    //       - Метод доступен по GET запросу на "/future-date"
+    @GetMapping("/future-date")
+    public String futureDate() {
+        LocalDate now = LocalDate.now();
+        Random random = new Random();
+        int randomNumberDays = random.nextInt(30);
+        LocalDate future = now.plusDays(randomNumberDays);
+        return "Случайная дата в будущем: " + future;
+    }
 }
